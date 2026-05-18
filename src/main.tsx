d8d5f9cefdacf7bd4ed0b14bbd34fa85ab2d8d04
Savins Bushman <savinskamau01@gmail.com>
2026-05-18 08:15:34 +0300

    Insights and analytics updated for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { I18nextProvider } from "react-i18next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import { getRouter } from "./router";
 import i18n from "./i18n";
 import "./styles.css";
@@ -12,6 +15,8 @@ createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <RouterProvider router={router} />
+      <Analytics />
+      <SpeedInsights />
     </I18nextProvider>
   </React.StrictMode>,
 );
